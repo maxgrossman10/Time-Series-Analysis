c520eafde6bfd8d944360b6b486857dce0e1b260
Max Grossman <maxgrossman14@gmail.com>
2023-09-14 11:03:01 -0700

    Remove functions and make the script linear

--- ts_with_gm_stock.py
+++ ts_with_gm_stock.py
@@ -1,32 +1,20 @@
 # %% GET DATA FROM YAHOO FINANCE AND VISUALIZE
 
-
-# Libraries
 import yfinance as yf
 import pandas as pd
 import datetime
 import matplotlib.pyplot as plt
+import pymannkendall as mk
 
+# Fetch GM stock data for the last 100 days
+end_date = datetime.date.today()
+start_date = end_date - datetime.timedelta(days=100)
+gm_data = yf.download("GM", start=start_date, end=end_date)
 
-def get_stock_data(ticker_symbol, days=100):
-    end_date = datetime.date.today()
-    start_date = end_date - datetime.timedelta(days=days)
-
-    # Fetch the stock data
-    stock_data = yf.download(ticker_symbol, start=start_date, end=end_date)
-    return stock_data
-
-
-def plot_stock_data(data, ticker_symbol):
-    data["Close"].plot(figsize=(10, 6))
-    plt.title(f"{ticker_symbol} Stock Close Price for Last 100 Days")
-    plt.xlabel("Date")
-    plt.ylabel("Close Price")
-    plt.grid(True)
-    plt.show()
-
-
-if __name__ == "__main__":
-    ticker = "GM"
-    gm_data = get_stock_data(ticker, 100)
-    plot_stock_data(gm_data, ticker)
+# Plot GM stock data
+gm_data["Close"].plot(figsize=(10, 6))
+plt.title("GM Stock Close Price for Last 100 Days")
+plt.xlabel("Date")
+plt.ylabel("Close Price")
+plt.grid(True)
+plt.show()
