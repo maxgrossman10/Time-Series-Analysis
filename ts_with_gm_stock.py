7d07de3f81cc805956ded827d15482cfbe04b60b
Max Grossman <maxgrossman14@gmail.com>
2023-09-14 11:21:26 -0700

    Seasonal MK & ACF

--- ts_with_gm_stock.py
+++ ts_with_gm_stock.py
@@ -4,7 +4,7 @@ import yfinance as yf
 import pandas as pd
 import datetime
 import matplotlib.pyplot as plt
-import pymannkendall as mk
+
 
 # Fetch GM stock data for the last 100 days
 end_date = datetime.date.today()
@@ -17,4 +17,24 @@ plt.title("GM Stock Close Price for Last 100 Days")
 plt.xlabel("Date")
 plt.ylabel("Close Price")
 plt.grid(True)
+plt.show()
+
+
+# %% SEASONAL MANN-KENDALL TEST
+
+
+from statsmodels.graphics.tsaplots import plot_acf
+import pymannkendall as mk
+
+
+# Apply Seasonal Mann-Kendall test and print the result
+result = mk.seasonal_test(gm_data["Close"], period=7)
+print(result)
+
+
+# Plot the autocorrelation function for 200 lagging time periods
+fig, ax = plt.subplots(figsize=(12, 6))
+acf = plot_acf(gm_data["Close"], lags=50, ax=ax)
+for line in acf.lines:
+    line.set_linewidth(0.5)
 plt.show()
