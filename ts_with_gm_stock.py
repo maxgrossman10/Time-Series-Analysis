1a4cbd8d1ae65745204258e11f5ce7e0ade2edd2
Max Grossman <maxgrossman14@gmail.com>
2023-09-14 11:47:27 -0700

    Seasonal Decomposition

--- ts_with_gm_stock.py
+++ ts_with_gm_stock.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 # Fetch GM stock data for the last 100 days
 end_date = datetime.date.today()
-start_date = end_date - datetime.timedelta(days=100)
+start_date = end_date - datetime.timedelta(days=365)
 gm_data = yf.download("GM", start=start_date, end=end_date)
 
 # Plot GM stock data
@@ -28,7 +28,7 @@ import pymannkendall as mk
 
 
 # Apply Seasonal Mann-Kendall test and print the result
-result = mk.seasonal_test(gm_data["Close"], period=7)
+result = mk.seasonal_test(gm_data["Close"], period=12)
 print(result)
 
 
@@ -37,4 +37,36 @@ fig, ax = plt.subplots(figsize=(12, 6))
 acf = plot_acf(gm_data["Close"], lags=50, ax=ax)
 for line in acf.lines:
     line.set_linewidth(0.5)
+plt.show()
+
+
+# %% SEASONAL DECOMPOSITION
+
+# Libraries
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+
+# Seasonal decomposition for 365 period
+result = seasonal_decompose(gm_data["Close"], model="additive", period=12)
+
+# Visualize decompositions
+plt.figure(figsize=(12, 8))
+# Observed plot
+plt.subplot(411)
+plt.plot(gm_data["Close"], label="Observed")
+plt.legend(loc="upper left")
+# Trend plot
+plt.subplot(412)
+plt.plot(result.trend, label="Trend")
+plt.legend(loc="upper left")
+# Seasonal plot
+plt.subplot(413)
+plt.plot(result.seasonal, label="Seasonal")
+plt.legend(loc="upper left")
+# Residual plot
+plt.subplot(414)
+plt.plot(result.resid, label="Residual")
+plt.legend(loc="upper left")
+
+# Display the plots
 plt.show()
